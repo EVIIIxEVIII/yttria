a0bf0e3d55ec3ef418b3189f883330b109914228
Kirill Katz <xnlyre@protonmail.com>
2025-06-18 14:18:21 +0300

    add linear oppacityu

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,7 +4,7 @@ layout(location = 1) in vec3 vPosObject;
 
 layout(location = 0) out vec4 outColor;
 
-layout(set = 1, binding = 0) uniform sampler3D dyeTex;
+layout(set = 0, binding = 4) uniform sampler3D dyeTex;
 
 layout(set = 0, binding = 0) uniform GlobalUbo {
     mat4  projection;
@@ -17,8 +17,6 @@ layout(push_constant) uniform Push {
     mat4 modelInv;
 } pc;
 
-// ---------------------------------------------------------------------------------
-// Slab method: ray–AABB intersection in object space (box = [-0.5,0.5]³)
 bool intersectBox(vec3 ro, vec3 rd, out float t0, out float t1)
 {
     vec3 invDir = 1.0 / rd;
@@ -39,22 +37,25 @@ void main()
     vec3 roObj = (pc.modelInv * vec4(camPosWorld, 1.0)).xyz;
     vec3 rdObj = normalize((pc.modelInv * vec4(rdWorld, 0.0)).xyz);
 
-    float tEnter, tExit;
-    if (!intersectBox(roObj, rdObj, tEnter, tExit))
+    float t0, t1;
+    if (!intersectBox(roObj, rdObj, t0, t1))
         discard;
+
+    float tEnter = max(t0, 0.0);
+    float tExit = t1;
 
     const float stepSize = 0.01;
     float t = tEnter;
     vec3  accum = vec3(0.0);
     float alpha = 0.0;
 
-    for (int i = 0; i < 256 && t < tExit && alpha < 0.99; ++i) {
+    for (int i = 0; i < 512 && t < tExit && alpha < 0.99; ++i) {
         vec3 pObj = roObj + rdObj * t;
         vec3 uvw  = pObj + 0.5;
 
-        float density = step( length(pObj) , 0.3 ) * 1.0;
+        float density = texture(dyeTex, uvw).r;
         float k   = 4.0;
-        float a   = 1.0 - exp(-k * density * stepSize);
+        float a = k * density * stepSize;
         accum += (1.0 - alpha) * vec3(density) * a;
         alpha += (1.0 - alpha) * a;
 
